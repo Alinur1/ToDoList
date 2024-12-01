75a354a36c10a81e077d3a6481984dd5cd2c2f48
Md. Alinur Hossain <alinur1160@gmail.com>
2024-12-01 22:34:26 +0600

    dependency injection of service
    -UserService
    -TaskListService

--- AppLayer/Program.cs
+++ AppLayer/Program.cs
@@ -1,3 +1,5 @@
+using BLL.Interfaces;
+using BLL.Services;
 using DAL.DataAccess;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,6 +12,9 @@ builder.Services.AddDbContext<ToDoContext>(options =>
 options.UseMySQL(connectionStrings));
 
 // Add services to the container.
+
+builder.Services.AddScoped<IUser, UserService>();
+builder.Services.AddScoped<ITaskList, TaskListService>();
 
 builder.Services.AddControllers();
 
