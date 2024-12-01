1cd0522ed012288c4b5eb8fa71d7b29a4ef80dde
Md. Alinur Hossain <alinur1160@gmail.com>
2024-12-01 20:19:31 +0600

    services added
    - TaskListService
    - UserService

---
+++ BLL/Services/TaskListService.cs
@@ -0,0 +1,65 @@
+﻿using BLL.Interfaces;
+using DAL.DataAccess;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class TaskListService : ITaskList
+    {
+        private readonly ToDoContext _context;
+
+        public TaskListService(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TaskList> AddTaskListAsync(TaskList taskList)
+        {
+            await _context.task_list.AddAsync(taskList);
+            await _context.SaveChangesAsync();
+            return taskList;
+        }
+
+        public async Task<bool> DeleteTaskListAsync(int id)
+        {
+            var existingTaskList = await _context.task_list.FindAsync(id);
+            if (existingTaskList != null)
+            {
+                _context.task_list.Remove(existingTaskList);
+                _context.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<IEnumerable<TaskList>> GetAllTaskListAsync()
+        {
+            return await _context.task_list.ToListAsync();
+        }
+
+        public async Task<TaskList> GetTaskListByIdAsync(int id)
+        {
+            return await _context.task_list.FindAsync(id);
+        }
+
+        public async Task<TaskList> UpdateTaskListAsync(TaskList taskList)
+        {
+            var existingTaskList = await _context.task_list.FindAsync(taskList.task_id);
+            if (existingTaskList != null)
+            {
+                existingTaskList.task_name = taskList.task_name;
+                existingTaskList.task_description = taskList.task_description;
+
+                _context.task_list.Update(existingTaskList);
+                await _context.SaveChangesAsync();
+            }
+            return taskList ?? throw new KeyNotFoundException("Task not found.");
+        }
+    }
+}
---
+++ BLL/Services/UserService.cs
@@ -0,0 +1,66 @@
+﻿using BLL.Interfaces;
+using DAL.DataAccess;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class UserService : IUser
+    {
+        private readonly ToDoContext _context;
+
+        public UserService(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> AddUserAsync(User user)
+        {
+            await _context.users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<bool> DeleteUserAsync(int id)
+        {
+            var existingUser = await _context.users.FindAsync(id);
+            if(existingUser != null)
+            {
+                _context.users.Remove(existingUser);
+                _context.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await _context.users.ToListAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            return await _context.users.FindAsync(id);
+        }
+
+        public async Task<User> UpdateUserAsync(User user)
+        {
+            var existingUser = await _context.users.FindAsync(user.user_id);
+            if(existingUser != null)
+            {
+                existingUser.full_name = user.full_name;
+                existingUser.email = user.email;
+                existingUser.password = user.password;
+
+                _context.users.Update(existingUser);
+                await _context.SaveChangesAsync();
+            }
+            return user ?? throw new KeyNotFoundException("User not found");
+        }
+    }
+}
