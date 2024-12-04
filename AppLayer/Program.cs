3b0cda06671ece51b9186812a0972bec78f021d7
Md. Alinur Hossain <alinur1160@gmail.com>
2024-12-04 13:24:04 +0600

    CORS added

--- AppLayer/Program.cs
+++ AppLayer/Program.cs
@@ -5,24 +5,34 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-//Configure connection string
+// Configure connection string
 var connectionStrings = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ToDoContext>(options =>
-options.UseMySQL(connectionStrings));
+    options.UseMySQL(connectionStrings));
 
 // Add services to the container.
-
 builder.Services.AddScoped<IUser, UserService>();
 builder.Services.AddScoped<ITaskList, TaskListService>();
-
 builder.Services.AddControllers();
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // React App
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
+
+app.UseCors("AllowSpecificOrigins");
 
 app.UseAuthorization();
 
